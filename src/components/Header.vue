ec8627cf43d6b935c11b7f054492f0a6ab99e847
michael <michaeldebs16@gmail.com>
2022-08-19 01:22:23 +0300

    did some adjustments to the heading

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,15 +2,10 @@
     <header
         class="h-[100vh] header-bg bg-no-repeat bg-cover grid grid-rows-[80%_20%] grid-cols-[20%_60%_20%] text-center">
         <div class="row-start-1 col-start-2 flex items-center justify-center flex-col">
-            <h1 class="text-5xl font-serif">Mint and Jade</h1>
+            <h1 class="text-5xl font-serif mb-3">Mint and Jade</h1>
             <h2 class="mb-5">Open from 9 AM till 1 AM</h2>
             <p>~By Ibrahim El-kari</p>
         </div>
     </header>
 </template>
 
-<script>
-export default {
-
-}
-</script>
