ad2e91999cd2f5893b7c893184b0375e9092347c
michael <michaeldebs16@gmail.com>
2022-08-19 11:47:43 +0300

    Bug fixes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header
-        class="h-[100vh] header-bg bg-no-repeat bg-cover grid grid-rows-[80%_20%] grid-cols-[20%_60%_20%] text-center">
+        class="h-[100vh] header-bg bg-no-repeat bg-cover grid grid-rows-[80%_20%] grid-cols-[15%_70%_15%] text-center">
         <div class="row-start-1 col-start-2 flex items-center justify-center flex-col">
             <h1 class="text-5xl font-serif mb-3">Mint & Jade</h1>
             <h2 class="mb-5">Open from 9 AM till 1 AM</h2>
