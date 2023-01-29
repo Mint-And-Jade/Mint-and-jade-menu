67c5c43ef094d39a281027eab32fdf370dcded97
michael <michaeldebs16@gmail.com>
2023-01-29 14:49:46 +0200

    Loading circle and Disabled submit

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -10,6 +10,22 @@
         </nav>
 
 
+       <!-- Loading Circle -->
+       <div role="status" class="h-[20vh] flex justify-center items-center" v-if="fetching">
+            <svg aria-hidden="true" class="w-12 h-12 mr-2 text-gray-200 animate-spin dark:text-gray-400 fill-[#59d460]"
+                viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor" />
+                <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill" />
+            </svg>
+            <span class="sr-only">Loading...</span>
+        </div>
+
+
+
         <!-- Edit -->
         <div v-if="currentTab == 'edit'">
             <!-- foreach section -->
@@ -25,9 +41,11 @@
                     <div v-for="(item, index) in category.items" :key="item._id"
                         class="flex  justify-around items-center mb-[1rem]">
                         <div class="flex justify-between w-[65%] inputs">
-                            <input class="w-[70%]" v-model="data[indexS].categories[indexC].items[index].name" @focus="itemEdited(data[indexS].categories[indexC].items[index])" >
+                            <input class="w-[70%]" v-model="data[indexS].categories[indexC].items[index].name"
+                                @focus="itemEdited(data[indexS].categories[indexC].items[index])">
                             <div class="">
-                                <input class="max-w-[3rem]"  v-model="data[indexS].categories[indexC].items[index].price" @focus="itemEdited(data[indexS].categories[indexC].items[index])">LL
+                                <input class="max-w-[3rem]" v-model="data[indexS].categories[indexC].items[index].price"
+                                    @focus="itemEdited(data[indexS].categories[indexC].items[index])">LL
                             </div>
                         </div>
                         <div id="" :data-itemId="item._id"
@@ -37,7 +55,8 @@
                 </div>
             </div>
         </div>
-        <div class="bg-[#59d460] px-[2rem] py-[1rem] rounded-lg cursor-pointer" @click="submitChanges()">Apply Changes</div>
+        <div id="submitBtn" class="bg-[#59d460] px-[2rem] py-[1rem] rounded-lg cursor-pointer" @click.once="submitChanges()">Apply Changes
+        </div>
     </main>
 </template>
 
@@ -96,6 +115,8 @@ export default {
                 window.scroll(0, window.localStorage.getItem('pos'))
                 window.localStorage.setItem('pos', 0)
             }, 100)
+
+            this.fetching = false
         },
         clickedAdd() {
             this.currentTab = 'add'
@@ -108,16 +129,16 @@ export default {
             this.editStyle = 'cursor-pointer text-[#59d460]'
         },
         deleteItem(sectionId, categoryId, itemId) {
-            fetch('https://zealous-cyan-katydid.cyclic.app/delete-item', { method: 'DELETE', body: JSON.stringify({ item_id: itemId }), headers: { 'Content-Type': 'application/json' }})
-            .then(() => {
-                window.localStorage.setItem('pos', window.scrollY)
-                location.reload()
-            })
+            fetch('https://zealous-cyan-katydid.cyclic.app/delete-item', { method: 'DELETE', body: JSON.stringify({ item_id: itemId }), headers: { 'Content-Type': 'application/json' } })
+                .then(() => {
+                    window.localStorage.setItem('pos', window.scrollY)
+                    location.reload()
+                })
         },
-        itemEdited(item){
+        itemEdited(item) {
             let inList = false
             for (let i = 0; i < this.editedItems.length; i++) {
-                if (item._id == this.editedItems[i]._id) {  
+                if (item._id == this.editedItems[i]._id) {
                     this.editedItems[i].name = item.name
                     this.editedItems[i].price = item.price
                     inList = true
@@ -128,11 +149,12 @@ export default {
             }
         },
         submitChanges() {
-            fetch('https://zealous-cyan-katydid.cyclic.app/edit-items', { method: 'PUT', body: JSON.stringify({ editedItems: this.editedItems }), headers: { 'Content-Type': 'application/json' }})
-            .then(() => {
+            document.querySelector('#submitBtn').style.backgroundColor = "#6e8978"
+            fetch('https://zealous-cyan-katydid.cyclic.app/edit-items', { method: 'PUT', body: JSON.stringify({ editedItems: this.editedItems }), headers: { 'Content-Type': 'application/json' } })
+                .then(() => {
 
-                location.reload()
-            })
+                    location.reload()
+                })
         }
     },
     data() {
@@ -144,7 +166,8 @@ export default {
             items: undefined,
             categories: undefined,
             sections: undefined,
-            editedItems: []
+            editedItems: [],
+            fetching: true
         }
     },
     created() {
