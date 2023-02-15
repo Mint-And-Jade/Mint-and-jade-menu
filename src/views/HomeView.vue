7d4ed77fc5de9cc6087bea8d42b8b95fbd96e5a1
michael <michaeldebs16@gmail.com>
2023-02-15 21:33:43 +0200

    Added comma in prices

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -56,7 +56,7 @@
                                                 <p class="ml-5"> {{ item.name }}</p>
                                             </div>
                                             <div class="w-[75px]">
-                                                {{ item.price }} LL
+                                                {{ item.price.toLocaleString() }} LL
                                             </div>
                                         </div>
                                     </div>
